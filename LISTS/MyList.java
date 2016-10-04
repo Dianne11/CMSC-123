6195b61998211b88064e2274fd6f44556b3249df
Dianne11 <diannedeiparine11@gmail.com>
2016-10-04 10:28:47 +0800

    Update MyList.java

--- LISTS/MyList.java
+++ LISTS/MyList.java
@@ -3,7 +3,7 @@ public interface MyList{
   //public MyNode(Object item);
   public void add(String item);
   public void add(int i, String item);
-  public void remove(String item);
+  public void remove(Object item);
   public void remove(int i);
   public String get(int i);
   public void set(int i, String item);
